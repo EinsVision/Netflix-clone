b1350b83ee2104f512a05a9f7ee2b67449b64e66
jungchangdae <einsvision@gmail.com>
2021-02-11 05:06:32 +0900

    React Router 설치

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -22,7 +22,7 @@ function Nav() {
                 <img className='nav__logo'
                      src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
                      alt=''
-                />
+                /> 
                 <img className='nav__avatar'
                      src='https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png'
                      alt=''
