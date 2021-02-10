bea8492092be5dfd3f53c2077800ba52801fae51
jungchangdae <einsvision@gmail.com>
2021-02-10 15:54:02 +0900

    front-end deploy on firebase

--- netflix-clone/src/HomeScreen.js
+++ netflix-clone/src/HomeScreen.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
-
+import Banner from './Banner';
+import requests from './Request';
+import Row from './Row';
 function HomeScreen() {
     return (
         <div className='homeScreen'>
           <Nav />
           
-          {/* Banner */}
+          <Banner />
+                    
+          <Row 
+            title='NETFLIX ORIGINALS'
+            fetchURL={requests.fetchNetflixOriginals}
+            isLargeRow
+          />
+          <Row 
+            title='Trending Now'
+            fetchURL={requests.fetchTrending} 
+          />
+          <Row 
+            title='Top Rated'
+            fetchURL={requests.fetchTopRated} 
+          />
+          <Row 
+            title='Action Movies'
+            fetchURL={requests.fetchActionMovies} 
+          />
+          <Row 
+            title='Comedy Movies'
+            fetchURL={requests.fetchComedyMovies} 
+          />
+          <Row 
+            title='Horror Movies'
+            fetchURL={requests.fetchHorrorMovies} 
+          />
+          <Row 
+            title='Romance Movies'
+            fetchURL={requests.fetchRomanceMovies} 
+          />
+          <Row 
+            title='Documentaries'
+            fetchURL={requests.fetchDocumentaries} 
+          />
 
-          {/* Row */}
         </div>
     )
 }
