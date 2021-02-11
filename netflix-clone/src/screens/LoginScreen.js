05b56d2fa3426c30acd010308e54782451ee8587
jungchangdae <einsvision@gmail.com>
2021-02-11 23:05:28 +0900

    sign up & sign in using firebase

--- netflix-clone/src/screens/LoginScreen.js
+++ netflix-clone/src/screens/LoginScreen.js
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
-import './LoginScreen.css'
+import React, {useState} from 'react';
+import './LoginScreen.css';
+import SignupScreen from './SignupScreen';
 
 function LoginScreen() {
     const [signIn, setSignIn] = useState(false);
@@ -12,24 +13,31 @@ function LoginScreen() {
                     src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' 
                     alt=''
                 />
-                <button className='loginScreen__button' >Sign In</button>
+                <button onClick={() => setSignIn(true)}
+                    className='loginScreen__button' >Sign In</button>
                 <div className='loginScreen__gradient'></div>
             </div>
 
             <div className='loginScreen__body'>
-                <>
+                {signIn ? (
+                    <SignupScreen />
+                ): (
+                    <>
                     <h1>Unlimited films, TV programmes and more.</h1>
                     <h2>Watch anywhere. Cancel at any time.</h2>
                     <h3>Raady to watch? Enter your email to create or restart your membership.</h3>
                     <div className='loginScreen__input'>
                         <form>
                             <input type='email' placeholder='Email Address'/>
-                            <button className='loginScreen__getStarted'>
+                            <button onClick={()=>setSignIn(true)} 
+                                className='loginScreen__getStarted'>
                                 GET STARTED
                             </button>
                         </form>
                     </div>
                 </>
+                )}
+                
             </div>
             
         </div>
